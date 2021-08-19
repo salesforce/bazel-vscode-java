f119a1dae636f62a5251b8fbe8c3ba015b730db3
alexei shmidman <alexeishmidman@gmail.com>
2021-08-20 00:59:25 +0300

    Switch to bazel-eclipe repo

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,18 @@ const gulp = require('gulp');
 const cp = require('child_process');
 const path = require('path');
 const fs = require('fs');
-const serverDir = path.join(__dirname, 'bazel-ls-eclipse');
-const mvnCmd = path.join(serverDir, mvnw());
+const bazelEclipseDir = path.join(__dirname, 'bazel-eclipse');
 
 gulp.task('build-plugin', (done) => {
   
-  if( fs.existsSync(mvnCmd) ){
-   cp.execSync('git fetch origin', {cwd: serverDir, stdio: [0, 1, 2]  });
-   cp.execSync('git pull origin --force', {cwd: serverDir, stdio: [0, 1, 2]  });
-  }else{
-   cp.execSync('git clone https://github.com/salesforce/bazel-ls-eclipse.git', { cwd: __dirname, stdio: [0, 1, 2] });
-  }
+  // if( fs.existsSync(mvnCmd) ){
+  //  cp.execSync('git fetch origin', {cwd: serverDir, stdio: [0, 1, 2]  });
+  //  cp.execSync('git pull origin --force', {cwd: serverDir, stdio: [0, 1, 2]  });
+  // }else{
+  //  cp.execSync('git clone https://github.com/salesforce/bazel-ls-eclipse.git', { cwd: __dirname, stdio: [0, 1, 2] });
+  // }
   
-  cp.execSync(`mvn clean package`, { cwd: serverDir, stdio: [0, 1, 2] });
+  cp.execSync(`mvn clean package`, { cwd: bazelEclipseDir, stdio: [0, 1, 2] });
   done();
 });
 
