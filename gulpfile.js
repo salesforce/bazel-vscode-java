83df254d94531aa800e0f7fa3f94553796dd1f22
Alexei Shmidman <alexeishmidman@gmail.com>
2021-08-25 22:37:11 +0300

    switch to BEF-JDT LS monorep (#10)
    * Switch to bazel-eclipe repo
    
    * Build with bazel-eclipse github repository
    
    * switch to bazel-eclipe master branch
    
    * correct ignorable folders and files in vsce

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,20 @@ const gulp = require('gulp');
 const cp = require('child_process');
 const path = require('path');
 const fs = require('fs');
-const serverDir = path.join(__dirname, 'bazel-ls-eclipse');
-const mvnCmd = path.join(serverDir, mvnw());
+const bazelEclipseDir = path.join(__dirname, 'bazel-eclipse');
 
 gulp.task('build-plugin', (done) => {
-  
-  if( fs.existsSync(mvnCmd) ){
-   cp.execSync('git fetch origin', {cwd: serverDir, stdio: [0, 1, 2]  });
-   cp.execSync('git pull origin --force', {cwd: serverDir, stdio: [0, 1, 2]  });
-  }else{
-   cp.execSync('git clone https://github.com/salesforce/bazel-ls-eclipse.git', { cwd: __dirname, stdio: [0, 1, 2] });
-  }
-  
-  cp.execSync(`mvn clean package`, { cwd: serverDir, stdio: [0, 1, 2] });
-  done();
+  fs.readdirSync(__dirname).forEach((file, index) => {
+    if (file.match('.*bazel-ls-vscode-.*\\.vsix')) {
+      fs.unlinkSync(file);
+    }
+  });
+
+ removeFolder(bazelEclipseDir);
+
+ cp.execSync('git clone https://github.com/salesforce/bazel-eclipse.git', { cwd: __dirname, stdio: [0, 1, 2] });
+ cp.execSync(`mvn clean package`, { cwd: bazelEclipseDir, stdio: [0, 1, 2] });
+ done();
 });
 
 function isWin() {
@@ -27,4 +27,18 @@ function isWin() {
 
 function mvnw() {
   return isWin() ? 'mvnw.cmd' : './mvnw';
+}
+
+function removeFolder(folder) {
+  if (fs.existsSync(folder)) {
+    fs.readdirSync(folder).forEach((file, index) => {
+      var child = path.join(folder, file);
+      if (fs.statSync(child).isDirectory()) {
+        removeFolder(child);
+      } else {
+        fs.unlinkSync(child);
+      }
+    });
+    fs.rmdirSync(folder);
+  }
 }
