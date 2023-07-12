72d2c8575259636159d6462d2634ad8f1f0e3cdc
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2023-07-12 19:17:43 +0200

    Polish

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,8 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import { BazelTaskProvider } from './bazelTaskProvider';
-
-let bazelTaskProvider: vscode.Disposable | undefined;
 
 export function activate(context: vscode.ExtensionContext) {
-	let panel: vscode.WebviewPanel | undefined = undefined;
-
-	console.log('Extension "bazelimport" is now active!');
-
-	bazelTaskProvider = vscode.tasks.registerTaskProvider(BazelTaskProvider.BazelType, new BazelTaskProvider());
-
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() { 
-	if (bazelTaskProvider) {
-		bazelTaskProvider.dispose();
-	}
+export function deactivate() {
 }
