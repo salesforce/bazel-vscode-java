040736da656190cbf84a96f4feed0f07d366d841
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2023-07-12 15:25:02 +0200

    WIP of packaging rework to new LS

---
+++ CONTRIBUTING.md
@@ -0,0 +1,19 @@
+# Contribution Guide
+
+## Setup
+
+For a proper development setup there are four components required:
+
+1. This VSCode extension (Bazel VSCode Java)
+2. The Bazel JDT Language Server extension
+3. The RedHat VSCode Java extension
+4. The Eclipse JDT Language Server
+
+The model we follow here is follows the pattern found in the RedHat VSCode Java extension.
+Allthough steps 3 and 4 are optional, they are highly recommended for a full setup which allows contributions upstream to those.
+
+All repositories must be cloned into the same parent directory, i.e. they all must be siblings and use the default directory name.
+
+
+## Release
+
--- gulpfile.js
+++ gulpfile.js
@@ -3,47 +3,129 @@
 
 const gulp = require('gulp');
 const cp = require('child_process');
+const decompress = require('gulp-decompress');
+const download = require('gulp-download');
+const rename = require('gulp-rename');
+const filter = require('gulp-filter');
+const gRegexRename = require('gulp-regex-rename');
+const request = require('request');
+const glob = require('glob');
+const fse = require('fs-extra');
 const path = require('path');
-const fs = require('fs');
-const bazelEclipseDir = path.join(__dirname, 'bazel-eclipse');
+const url = require("url");
+const argv = require('minimist')(process.argv.slice(2));
+const BAZEL_ECLIPSE_DIR = '../bazel-eclipse';
+const BAZEL_ECLIPSE_LATEST_URL = "https://opensource.salesforce.com/bazel-eclipse/latest/p2-repository.zip";
+const NON_NPM_REPOSITORY_RE = new RegExp(
+  String.raw`"resolved":\s*"https://(?!(registry\.npmjs\.org\/?))`,
+  "g"
+);
 
-gulp.task('build-plugin', (done) => {
-  fs.readdirSync(__dirname).forEach((file, index) => {
-    if (file.match('.*bazel-vscode-.*\\.vsix')) {
-      fs.unlinkSync(file);
-    }
-  });
+// a little helper to drop OSGi versions from bundle jar file name
+const DROP_JAR_VERSION = gRegexRename(/_\d+\.\d+\.\d+(\.[^\.]+)?\.jar/, '.jar');
 
-  if (!fs.existsSync(bazelEclipseDir)) {
-    fs.mkdirSync(bazelEclipseDir);
-  }
+// read the package.json once so we can use it in the gulp script
+const packageJson = JSON.parse(fse.readFileSync("./package.json").toString());
 
-  // del.sync(bazelEclipseDir + '/**', { force: true });
-  fs.rmdirSync(bazelEclipseDir, {recursive: true});
+// we only need the headless jars of the Bazel JDT Language Server extension
+const declaredServerJars = new Set(packageJson.contributes.javaExtensions.map(path => path.split('/').reverse()[0]));
+const jarIsIncludedInPackageJson = filter(file => {
+  return declaredServerJars.has(file.basename);
+});
 
-  cp.execSync('git clone https://github.com/salesforce/bazel-eclipse.git', { cwd: __dirname, stdio: [0, 1, 2] });
-  cp.execSync(`mvn clean package`, { cwd: bazelEclipseDir, stdio: [0, 1, 2] });
-  renameTarget('com.salesforce.b2eclipse.jdt.ls');
-  renameTarget('com.salesforce.bazel.eclipse.common');
-  renameTarget('com.salesforce.bazel-java-sdk');
+gulp.task('download_server', function (done) {
+  downloadServerImpl();
   done();
+});
+
+gulp.task('build_server', function (done) {
+  buildServerImpl();
+  done();
+});
+
+gulp.task('build_or_download', function (done) {
+  if (!fse.existsSync(BAZEL_ECLIPSE_DIR)) {
+    console.log('NOTE: bazel-eclipse is not found as a sibling directory, downloading the latest snapshot of the Bazel JDT Language Server extension...');
+    downloadServerImpl();
+  }
+  else {
+    buildServerImpl();
+  }
+  done();
+});
+
+gulp.task('prepare_pre_release', function (done) {
+	const stableVersion = packageJson.version.match(/(\d+)\.(\d+)\.(\d+)/);
+	const major = stableVersion[1];
+	const minor = stableVersion[2];
+	const date = new Date();
+	const month = date.getMonth() + 1;
+	const day = date.getDate();
+	const hours = date.getHours();
+	const patch = `${date.getFullYear()}${prependZero(month)}${prependZero(day)}${prependZero(hours)}`;
+	const insiderPackageJson = Object.assign(packageJson, {
+		version: `${major}.${minor}.${patch}`,
+	});
+	fse.writeFileSync("./package.json", JSON.stringify(insiderPackageJson, null, 2));
+	done();
+});
+
+gulp.task('repo_check', function (done) {
+	const data = fse.readFileSync("./package-lock.json", { encoding: "utf-8" });
+
+	if (NON_NPM_REPOSITORY_RE.test(data)) {
+		done(new Error("Found references to the internal registry in the file package-lock.json. Please fix it with replacing all URLs using 'https://registry.npmjs.org'!"));
+	} else {
+		done();
+	}
 });
 
 function isWin() {
   return /^win/.test(process.platform);
 }
 
-function mvnw() {
-  return isWin() ? 'mvnw.cmd' : './mvnw';
+function isMac() {
+  return /^darwin/.test(process.platform);
 }
 
-function renameTarget(plugin) {
-  bundlesPath = path.join(bazelEclipseDir, 'bundles');
-  pluginPath = path.join(bundlesPath, plugin);
-  targetPath = path.join(pluginPath, 'target');
-  sourceFileName = fs.readdirSync(targetPath).find(file => file.match(plugin + '-.*-SNAPSHOT\\.jar'));
-  sourceFile = path.join(targetPath, sourceFileName);
-  targetFile = path.join(targetPath, plugin + '.jar');
-  fs.renameSync(sourceFile, targetFile);
+function isLinux() {
+  return /^linux/.test(process.platform);
+}
+
+function mvnw() {
+  return isWin() ? "mvnw.cmd" : "./mvnw";
+}
+
+function prependZero(num) {
+	if (num > 99) {
+		throw new Error("Unexpected value to prepend with zero");
+	}
+	return `${num < 10 ? "0" : ""}${num}`;
+}
+
+function downloadServerImpl() {
+  fse.removeSync('./server');
+  download(BAZEL_ECLIPSE_LATEST_URL)
+    .pipe(decompress())
+    .pipe(filter(['plugins/*.jar']))
+    .pipe(rename(function (path) {
+      return {
+        dirname: "", // flatten
+        basename: path.basename,
+        extname: path.extname
+      };
+    }))
+    .pipe(DROP_JAR_VERSION)
+    .pipe(jarIsIncludedInPackageJson)
+    .pipe(gulp.dest('./server'));
+}
+
+function buildServerImpl() {
+  fse.removeSync('./server');
+  cp.execSync(mvnw() + ' clean package -DskipTests=true', { cwd: BAZEL_ECLIPSE_DIR, stdio: [0, 1, 2] });
+  gulp.src(BAZEL_ECLIPSE_DIR + '/releng/p2repository/target/repository/plugins/*.jar')
+    .pipe(DROP_JAR_VERSION)
+    .pipe(jarIsIncludedInPackageJson)
+    .pipe(gulp.dest('./server'));
 }
 
