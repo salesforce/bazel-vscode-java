e1638b4692198976f37e42592194306a1c45aad6
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2023-09-21 13:01:53 +0200

    Log output of JDTLS during build
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,20 +1,26 @@
 import * as cp from 'child_process';
+import * as fs from 'fs';
+import { glob } from 'glob';
 import * as os from 'os';
 import * as path from 'path';
+import { env } from 'process';
 
 import { downloadAndUnzipVSCode, resolveCliArgsFromVSCodeExecutablePath, runTests } from '@vscode/test-electron';
 
 async function main() {
+	// folder containing the Extension Manifest package.json
+	const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+
+	// path to the extension test runner script
+	const extensionTestsPath = path.resolve(__dirname, './suite/index');
+
+	// path to the sampel project for testing
+	const testProjectPath: string = path.resolve(extensionDevelopmentPath, './src/test/projects/small/');
+
+	// temp area for test user data
+	const testUserDataPath: string = path.resolve(os.tmpdir(), './suite-projects-small/');
+
 	try {
-		// folder containing the Extension Manifest package.json
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-
-		// path to the extension test runner script
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
-
-		// path to the sampel project for testing
-		const testProjectPath: string = path.resolve(extensionDevelopmentPath, './src/test/projects/small/');
-
 		// prepare VS Code (use latest stable)
 		const vscodeExecutablePath = await downloadAndUnzipVSCode('stable');
 		const [cliPath, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
@@ -29,8 +35,13 @@ async function main() {
 			}
 		);
 
+		// ensrue the user data area is empty
+		if (fs.existsSync(testUserDataPath)) {
+			fs.rmdirSync(testUserDataPath, {recursive: true});
+		}
+
 		// run the integration test with spawned VS Code
-		console.log(`Running test suite with project ${testProjectPath} ...`);
+		console.log(`Running test suite with\n\tproject: ${testProjectPath}\n\tuser-data-dir: ${testUserDataPath}\n...`);
 		await runTests({
 			vscodeExecutablePath,
 			extensionDevelopmentPath,
@@ -38,13 +49,42 @@ async function main() {
 			launchArgs: [
 				'--disable-workspace-trust',
 				'--user-data-dir',
-				`${os.tmpdir()}`,
+				testUserDataPath,
 				testProjectPath
 			]
 		});
 	} catch (err) {
-		console.error('Failed to run tests');
-		console.error(err);
+		console.log('\n\n\n');
+		console.log('********************************************************************************************');
+		console.error(`Failed to run tests: ${err}`);
+		console.log('********************************************************************************************');
+		console.log('\n\n\n');
+
+		// try to locate Java LS log
+		const lsLogs = await glob('**/*Java.log', { cwd: testUserDataPath, withFileTypes: true });
+		if (lsLogs.length > 0) {
+			if (env['PRINT_JDTLS_LOGS'] === 'true') {
+				console.log('Output of JDTLS log below:');
+				lsLogs.forEach(log => {
+					console.log(`> cat ${log.fullpath()}`);
+					try {
+						const data = fs.readFileSync(log.fullpath(), 'utf8');
+						console.log(data);
+					} catch (err) {
+						console.error(err);
+					}
+					console.log(`\n\n`);
+				});
+			} else {
+				console.log('Set PRINT_JDTLS_LOGS=true to show the following JDTLS log automatically:');
+				lsLogs.forEach(log => {
+					console.log(`\tcat ${log.fullpath()}`);
+				});
+			}
+		} else {
+			console.warn("No logs from JDTLS found!");
+		}
+
 		process.exit(1);
 	}
 }
